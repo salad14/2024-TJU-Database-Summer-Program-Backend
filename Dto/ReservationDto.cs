4b5f1fd18a8e44105d8d5180d61d6f88bfb12f60
kwzr <1966650923@qq.com>
2024-08-27 23:13:57 +0800

    修改部分id的string问题

--- Dto/ReservationDto.cs
+++ Dto/ReservationDto.cs
@@ -3,7 +3,7 @@ public class ReservationDto
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public decimal PaymentAmount { get; set; }  // 支付金额
-    public string UserId { get; set; }  // 用户ID
+    public int UserId { get; set; }  // 用户ID
     public string VenueId { get; set; }  // 场地ID
     public string AvailabilityId { get; set; }  // 开放时间段ID
     public string ReservationItem { get; set; }  // 预约项目描述
--- Models/Venue.cs
+++ Models/Venue.cs
@@ -4,7 +4,7 @@ namespace VenueBookingSystem.Models
 {
     public class Venue
     {
-        public int VenueId { get; set; } // 场地ID
+        public string VenueId { get; set; } // 场地ID
         public required string Name { get; set; } // 场地名称
         public required string Type { get; set; } // 场地类型
         public int Capacity { get; set; } // 容纳人数
