02c535c2b017b6731e8a3b78fb7cfd42107e8da3
donggua227 <taiyu1204@outlook.com>
2024-09-01 20:31:26 +0800

    Update VenueController.cs

--- Controllers/VenueController.cs
+++ Controllers/VenueController.cs
@@ -25,6 +25,30 @@ namespace VenueBookingSystem.Controllers
             var venues = _venueService.GetAllVenues();
             return Ok(venues);
         }
+        // 获取所有场地信息
+        [HttpGet("GetAllVenueInfos")]
+        public IActionResult GetAllVenueInfos()
+        {
+            try
+            {
+                var venueInfos = _venueService.GetAllVenueInfos();
+                return Ok(new 
+                { 
+                    state = 1, 
+                    data = venueInfos, 
+                    info = "" 
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new 
+                { 
+                    state = 0, 
+                    data = (object)null, 
+                    info = "An error occurred while retrieving venue information: " + ex.Message 
+                });
+            }
+        }
 
         // 添加新场地
         [HttpPost("AddVenue")]
@@ -34,6 +58,122 @@ namespace VenueBookingSystem.Controllers
             return Ok("场地添加成功");
         }
 
-        // 其他场地相关操作...
+        // 新增：获取所有不同ID的场地信息
+        [HttpGet("GetAllVenueDetails")]
+        public IActionResult GetAllVenueDetails()
+        {
+            try
+            {
+                var venueDetails = _venueService.GetAllVenueDetails();
+                return Ok(new 
+                { 
+                    state = 1, 
+                    data = venueDetails, 
+                    info = "" 
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new 
+                { 
+                    state = 0, 
+                    data = (object)null, 
+                    info = "An error occurred while retrieving data: " + ex.Message 
+                });
+            }
+        }
+
+        // 新增：获取所有维修记录及其相关设备和场地信息
+        [HttpGet("GetAllRepairRecords")]
+        public IActionResult GetAllRepairRecords()
+        {
+            try
+            {
+                var repairRecords = _venueService.GetAllRepairRecords();
+
+                // 成功时返回
+                return Ok(new 
+                { 
+                    state = 1, 
+                    data = repairRecords, 
+                    info = "" 
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new 
+                { 
+                    state = 0, 
+                    data = (object)null, 
+                    info = "An error occurred while retrieving data: " + ex.Message 
+                });
+            }
+        }
+        // 获取指定场地的详细信息
+        [HttpGet("GetVenueDetails")]
+        public IActionResult GetVenueDetails([FromQuery] string venueId)
+        {
+            try
+            {
+                var venueDetails = _venueService.GetVenueDetails(venueId);
+                if (venueDetails == null)
+                {
+                    return Ok(new 
+                    { 
+                        state = 0, 
+                        data = (object)null, 
+                        info = "Venue not found" 
+                    });
+                }
+                return Ok(new 
+                { 
+                    state = 1, 
+                    data = venueDetails, 
+                    info = "" 
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new 
+                { 
+                    state = 0, 
+                    data = (object)null, 
+                    info = "An error occurred while retrieving venue details: " + ex.Message 
+                });
+            }
+        }
+        // 获取指定设备的详细信息
+        [HttpGet("GetDeviceDetails")]
+        public IActionResult GetDeviceDetails([FromQuery] string equipmentId)
+        {
+            try
+            {
+                var deviceDetails = _venueService.GetDeviceDetails(equipmentId);
+                if (deviceDetails == null)
+                {
+                    return Ok(new 
+                    { 
+                        state = 0, 
+                        data = (object)null, 
+                        info = "Device not found" 
+                    });
+                }
+                return Ok(new 
+                { 
+                    state = 1, 
+                    data = deviceDetails, 
+                    info = "" 
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new 
+                { 
+                    state = 0, 
+                    data = (object)null, 
+                    info = "An error occurred while retrieving device details: " + ex.Message 
+                });
+            }
+        }
     }
 }
