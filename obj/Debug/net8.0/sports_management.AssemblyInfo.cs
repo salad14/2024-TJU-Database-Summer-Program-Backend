5b080e8a0107ab70cbdc3eaa8286a61b159d9916
Cintia Chen <cintia1362873053@gmail.com>
2024-09-04 14:22:29 +0800

    最新功能完善

--- obj/Debug/net8.0/sports_management.AssemblyInfo.cs
+++ obj/Debug/net8.0/sports_management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sports_management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dadee538b6106a74550caf63be171d00dc40f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea90ce7af24acff46d3b90d3b1e6a19cff79ba4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("sports_management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sports_management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
