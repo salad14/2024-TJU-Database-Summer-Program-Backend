ae4d2e7f7c80390c9305738f08a802f12f79a9f4
donggua227 <taiyu1204@outlook.com>
2024-09-04 15:52:13 +0800

    Update VenueDto.cs

--- Dto/VenueDto.cs
+++ Dto/VenueDto.cs
@@ -3,6 +3,7 @@ namespace VenueBookingSystem.Models
     // 定义 VenueDto 类
     public class VenueDto
     {
+        public string VenueId { get; set; } // 场地ID
         public required string Name { get; set; }  // 场地名称
         public required string Type { get; set; }  // 场地类型
         public int Capacity { get; set; }  // 场地容纳人数
