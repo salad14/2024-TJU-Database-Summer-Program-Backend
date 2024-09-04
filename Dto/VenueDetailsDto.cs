f327c12daac9facd80f83ed77ed55b90c30ee04b
donggua227 <taiyu1204@outlook.com>
2024-09-04 18:49:07 +0800

    Update VenueDetailsDto.cs

--- Dto/VenueDetailsDto.cs
+++ Dto/VenueDetailsDto.cs
@@ -1,4 +1,3 @@
-
 namespace VenueBookingSystem.Models
 {
     public class VenueDetailsDto
@@ -8,21 +7,21 @@ namespace VenueBookingSystem.Models
         public string Type { get; set; } // 运动类型
         public int Capacity { get; set; } // 可容纳人数
         public string Status { get; set; } // 场地状态
-        public IEnumerable<VenueAvailabilityDto> OpenTime { get; set; } // 开放时间信息
         public IEnumerable<EquipmentDto> VenueDevices { get; set; } // 设备信息
-    }
-
-    public class VenueAvailabilityDto
-    {
-        public string StartTime { get; set; } // 开始时间
-        public string EndTime { get; set; } // 结束时间
-        public int RemainingCapacity { get; set; } // 剩余容量
-        public decimal Price { get; set; } // 场地价格
+        public IEnumerable<VenueMaintenanceDto> MaintenanceRecords { get; set; } // 保养记录
     }
 
     public class EquipmentDto
     {
         public string EquipmentId { get; set; } // 设备ID
         public string EquipmentName { get; set; } // 设备名称
+    }
+
+    public class VenueMaintenanceDto
+    {
+        public string VenueMaintenanceId { get; set; } // 保养ID
+        public DateTime MaintenanceStartDate { get; set; } // 保养开始时间
+        public DateTime MaintenanceEndDate { get; set; } // 保养结束时间
+        public string Description { get; set; } // 保养描述
     }
 }
