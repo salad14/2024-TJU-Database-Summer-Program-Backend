6bf3a816697fe246edf58b86b32320c3ac6b6edb
Cintych930 <155135476+cintychens@users.noreply.github.com>
2024-09-04 16:48:01 +0800

    最后的修改

--- Models/UserReservation.cs
+++ Models/UserReservation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace VenueBookingSystem.Models
 {
@@ -8,8 +8,8 @@ namespace VenueBookingSystem.Models
         public required string UserId { get; set; } // 用户ID
         public DateTime? CheckInTime { get; set; } // 用户签到时间
         public required string Status { get; set; } // 预约状态
-        public int NumOfPeople { get; set; } // 预约人数
 
+        public int NumOfPeople { get; set; } // 预约人数
         // 导航属性
         public User User { get; set; } // 关联到用户
         public Reservation Reservation { get; set; } // 关联到预约记录
